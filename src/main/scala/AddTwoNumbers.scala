34710da4b59c0b56e3c93b6f1cb65d23641369c9
sambeth <sambethslim@gmail.com>
2020-09-22 17:32:36 +0000

    created methods to calculate length of linked list, difference in length and reversal of link list

--- src/main/scala/AddTwoNumbers.scala
+++ src/main/scala/AddTwoNumbers.scala
@@ -5,3 +5,35 @@ class ListNode(_x: Int = 0, _next: ListNode = null) {
   override def toString: String = s"$x -> $next"
 }
 
+object Solution {
+
+  def len(l: ListNode): Int = {
+    var counter: Int = 0
+    var node = l
+
+    while(node != null) {
+      counter += 1
+      node = node.next
+    }
+    counter
+  }
+
+  def differenceInLength(l1: ListNode, l2: ListNode): Int = {
+    val diff = len(l1) - len(l2)
+    diff.abs
+  }
+
+  def reverseList(head: ListNode): ListNode = {
+    var prev: ListNode = null
+    var curr: ListNode = head
+    var next: ListNode = null
+
+    while (curr != null) {
+      next = curr.next
+      curr.next = prev
+      prev = curr
+      curr = next
+    }
+    prev
+  }
+}
