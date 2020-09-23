0a445b4e77a6646d9ebac532effd8e22b8de8ff5
sambeth <sambethslim@gmail.com>
2020-09-23 00:52:29 +0000

    added leet code solution with simplified version

--- src/main/scala/AddTwoNumbers.scala
+++ src/main/scala/AddTwoNumbers.scala
@@ -128,6 +128,77 @@ object Solution {
     }
 
     // return reverse linked list
-    reverseList(finalLinkedList)
+    finalLinkedList
   }
 }
+
+//NOTE: above is my implementation of adding two linked list from scratch
+//and below is the solution for leet code in simplified version
+//LEET CODE SOLUTION BELOW
+
+//object Solution {
+//
+//  def addTwoNumbers(l1: ListNode, l2: ListNode) = {
+//    var first: ListNode = l1
+//    var second: ListNode = l2
+//
+//    // variables for adding two linked lists
+//    var p, q, tempNode, prevNode, finalLinkedList: ListNode = null
+//    var addition, divideBy10, remainder, carry, value: Int = 0
+//
+//    p = first
+//    q = second
+//
+//    while (p != null || q != null) {
+//
+//      var x = if (p != null) p.x else 0
+//      var y = if (q != null) q.x else 0
+//      addition = x + y
+//      addition = addition + carry
+//
+//      // after carry value is used revert it to zero
+//      carry = 0
+//
+//      divideBy10 = addition / 10
+//
+//      if (divideBy10 > 0) {
+//        remainder = addition % 10
+//        carry = divideBy10
+//        value = remainder
+//      } else {
+//        value = addition
+//      }
+//
+//      // create new node
+//      tempNode = new ListNode(value)
+//
+//      // obviously if this is the first run then set the head of the final linked list
+//      if (finalLinkedList == null) {
+//        finalLinkedList = tempNode
+//
+//        // if this is not the first run, which means the head has already
+//        // been created then continue the link
+//      } else {
+//        prevNode.next = tempNode
+//      }
+//
+//      // ready previous node for next iteration
+//      prevNode = tempNode
+//
+//      // move to next node
+//      p = if (p != null) p.next else null
+//      q = if (q != null) q.next else null
+//    }
+//
+//    // At the end of the while loop in carry is greater than zero
+//    // then obviously that value needs to be final node
+//    // otherwise no node will be create making the linked list
+//    // deficient of one node
+//    if (carry > 0) {
+//      tempNode.next = new ListNode(carry)
+//    }
+//
+//    // return reverse linked list
+//    finalLinkedList
+//  }
+//}
